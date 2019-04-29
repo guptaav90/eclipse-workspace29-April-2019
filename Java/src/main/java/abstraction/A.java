1bf45a4d6bddb622437a933970f5d565552f9124
Avi Gupta <gupta.av@HCL.COM>
2019-04-29 17:06:40 +0530

    newBranch

--- Java/src/main/java/abstraction/A.java
+++ Java/src/main/java/abstraction/A.java
@@ -13,7 +13,7 @@ public abstract  class A { /*1.If you declare abstract method in a class, then t
 	
 	public void Neft() {//This is non abstract method
 		
-		System.out.println("NEFT Method");
+		System.out.println("Neft Method");
 		
 	}
 		
